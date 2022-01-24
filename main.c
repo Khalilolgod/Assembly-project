f3ef0e4375a3bc9352359633bedd239c058a225e
khalilolgod <eyebiman@gmail.com>
2022-01-25 02:04:30 +0330

    matrix mul test

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void getMatrix(int* matrix, int y,int x){
 void printMatrix(int* matrix, int y,int x){
     for(int i = 0 ; i < y ; i++){
         for(int j = 0 ; j < x; j++){
-            printf("%d ",*((matrix+y*i)+j));
+            printf("%d ",*((matrix+x*i)+j));
         }
         printf("\n");
     }
@@ -20,26 +20,35 @@ void printMatrix(int* matrix, int y,int x){
 
 int main(){
     //memory
-    int A [625];
-    int B [625];
-    int Ay;
-    int Ax;
-    int By;
-    int Bx;
-    printf("getting A matrix axis (y*x)\n");
-    printf("Enter y : ");
-    scanf("%d",&Ay);
-    printf("Enter x : ");
-    scanf("%d",&Ax);
+    int A [625]= {2,3,4,8,9,10};
+    int B [625] = {1,2,3,4,5,6};
+    int Ay = 3;
+    int Ax = 2;
+    int By = 2;
+    int Bx = 3;
+    
+    int tmp[625];
+    int tmpY;
+    int tmpX;
+    
+    //-------------------------------
+    // A * B
+    tmpY = Ay;
+    tmpX = Bx;
+    for(int i = 0 ; i < Ay; i++){
+        for(int j = 0; j < Bx; j++){
+            *((tmp+tmpX*i)+j) = 0;
+            for(int k = 0 ; k < Ax; k++){
+                *((tmp+tmpX*i)+j) += *((A+Ax*i)+k) * *((B+Bx*k)+j);
+            }
+        }
+    }
 
-    // printf("getting A matrix axis (y*x)\n");
-    // printf("Enter y : ");
-    // scanf("%d",&By);
-    // printf("Enter x : ");
-    // scanf("%d",&Bx);
 
-    getMatrix(A,Ay,Ax);
+    // getMatrix(A,Ay,Ax);
     printMatrix(A,Ay,Ax);
+    printMatrix(B,By,Bx);
+    printMatrix(tmp,tmpY,tmpX);
     
     
 }
